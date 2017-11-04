8f05ef82492ae27ca4d71859f5109e5f0b5625c9
Marvin Liu <superliuwr@gmail.com>
2017-11-04 22:57:59 +1100

    Add CLI options

--- lib/emitter/cli.rb
+++ lib/emitter/cli.rb
@@ -3,8 +3,14 @@ require "emitter"
 
 module Emitter
   class CLI < Thor
+    option :stream, :required => true, :aliases => :s
+    option :data, :required => true, :aliases => :d
+    option :zip, :type => :boolean, :default => false, :aliases => :z
     desc "emitter", "AWS Kinesis Stream testing data producer"
-    def hello
+    def produce()
+      puts options[:stream]
+      puts options[:data]
+      puts options[:zip]
       puts Emitter.hello
     end
   end
